42c31975d7fde3b7fa7e48249542b3361523a007
Brad Alexander <brad.alexander913@gmail.com>
2019-09-05 01:49:52 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailAddressParser
     @emails = email_string.split(/[\s,']/)
     @emails.each_with_index do |address,index|
       if address == "" 
-        @email.delete_At(index)
+        @email.delete_at(index)
       binding.pry  
       end
     end 
